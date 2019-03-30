3fe31e39b77a90f6ffddc55e4c08987685885fab
melih <melihyayla@gmail.com>
2019-03-31 00:34:29 +0300

    Çeyiz sandığı sayfası tamamlandı, sadece progress bar düzelecek ve api bağlanacak.
    Product Detail sayfasındaki işlemlerin kaydı hakkında bilgi eksik. Nasıl yol izlenmeli belirtilmeli.
    
    Blurred Shadow Backgroundlar bekleniyor.

--- app/src/main/java/com/ceyizlistesi/ceyizlistesi/Product.java
+++ app/src/main/java/com/ceyizlistesi/ceyizlistesi/Product.java
@@ -10,6 +10,7 @@ import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.mbms.StreamingServiceInfo;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -28,7 +29,8 @@ public class Product extends AppCompatActivity {
     ProgressBar progressBar;
     LinearLayout prodcutButton, activeProducts, passiveProducts, demoLinear;
     Drawable unchecked, checked, detail_arrow;
-    int id, piece;
+    int id, piece, price;
+    String productName;
 
 
     @Override
@@ -37,7 +39,9 @@ public class Product extends AppCompatActivity {
         setContentView(R.layout.activity_product);
 
         String str = getIntent().getStringExtra("Value");
-        id = 0 ;
+        id = 0;
+        price=0 ;
+        productName = "Ürün";
         progressBar = findViewById(R.id.circularProgressbar);
         prodcutButton = findViewById(R.id.new_product_button);
 
@@ -83,14 +87,7 @@ public class Product extends AppCompatActivity {
         final LinearLayout priceNumber = mView.findViewById(R.id.price_linear_layout);
         final LinearLayout detailPage = mView.findViewById(R.id.detail_page_linear);
 
-        detailPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Product.this, ProductDetail.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+
 
         final EditText productNameEditText, priceEditText;
         ImageView decreaseButton, increaseButton, closeButton;
@@ -173,9 +170,6 @@ public class Product extends AppCompatActivity {
         Log.i("Productname", "Deneme");
 
 
-
-
-
         priceEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
@@ -188,13 +182,41 @@ public class Product extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String productName = productNameEditText.getText().toString();
+                productName = productNameEditText.getText().toString();
 
-                createProduct(activeProducts, productName, piece);
+
+                if(!priceEditText.getText().toString().isEmpty())
+                    price =  Integer.parseInt(priceEditText.getText().toString());
+
+
+                createProduct(activeProducts, productName, piece, price);
                 dialog.dismiss();
             }
         });
 
+
+
+
+
+
+        detailPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!priceEditText.getText().toString().isEmpty())
+                    price =  Integer.parseInt(priceEditText.getText().toString());
+
+                productName = productNameEditText.getText().toString();
+
+                Intent intent = new Intent(Product.this, ProductDetail.class);
+                intent.putExtra("price", price);
+                intent.putExtra("piece", piece);
+                intent.putExtra("productName", productName);
+
+                startActivity(intent);
+                dialog.dismiss();
+            }
+        });
 
 
         dialog.show();
@@ -203,15 +225,11 @@ public class Product extends AppCompatActivity {
 
     }
 
-    public void createProduct(LinearLayout parent, String productName, int piece){
+    public void createProduct(LinearLayout parent, final String myProductName, final int myPiece, final int myPrice){
 
         final float scale = getApplicationContext().getResources().getDisplayMetrics().density;
         int pixels = (int) (40 * scale + 0.5f);
         int pixels2 = (int) (25 * scale + 0.5f);
-
-
-        //ViewGroup.LayoutParams mainLayoutParams = parent.getLayoutParams();
-        //mainLayoutParams.height = parent.getMeasuredHeight() + pixels;
 
 
         final LinearLayout newProduct = new LinearLayout(this);
@@ -221,27 +239,18 @@ public class Product extends AppCompatActivity {
         layoutParams.setMargins(10,10,10,10);
 
         newProduct.setLayoutParams(layoutParams);
-        //newProduct.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, pixels));
 
         newProduct.setBackground(getResources().getDrawable(R.drawable.product_linear_background));
         newProduct.setOrientation(LinearLayout.HORIZONTAL);
 
 
         final ImageView imageView = new ImageView(getApplicationContext());
-
         imageView.setImageDrawable(unchecked);
-
-        LinearLayout.LayoutParams imageParams    = new LinearLayout.LayoutParams( pixels2 , pixels2);
-
+        final LinearLayout.LayoutParams imageParams    = new LinearLayout.LayoutParams( pixels2 , pixels2);
         layoutParams.setMargins(15,10,15,10);
         layoutParams.weight = 1;
-
         imageView.setLayoutParams(imageParams);
-
         final LinearLayout.LayoutParams productsParams    = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-
-
 
 
             imageView.setOnClickListener(new View.OnClickListener() {
@@ -269,88 +278,53 @@ public class Product extends AppCompatActivity {
                 }
             });
 
-
-
-
-
         newProduct.addView(imageView);
-
         TextView tv1 = new TextView(getApplicationContext());
-
-
         LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
         textParams.weight = 4;
-
-       textParams.setMargins(10,10,10,10);
-
+        textParams.setMargins(10,10,10,10);
         tv1.setTypeface(Typeface.create("gordita_regular", Typeface.NORMAL));
-
         tv1.setTextColor(Color.parseColor("#474167"));
-
         tv1.setLayoutParams(textParams);
 
 
 
         newProduct.addView(tv1);
-
+        String myPN = productName;
         if(piece>1){
-            productName += "(" + piece + ")";
+            myPN += "(" + piece + ")";
         }
         else{
             piece = 0;
         }
 
 
-        tv1.setText(productName);
-
+        tv1.setText(myPN);
         ImageView detail_arrow_image_view = new ImageView(getApplicationContext());
-
-        LinearLayout.LayoutParams imageParams2    = new LinearLayout.LayoutParams( pixels2 , pixels2);
-
+        LinearLayout.LayoutParams imageParams2  = new LinearLayout.LayoutParams( pixels2*90/100 , pixels2*90/100);
         imageParams2.setMargins(5,10,15,10);
-
         imageParams.weight = 1;
-
         detail_arrow_image_view.setLayoutParams(imageParams2);
-
         detail_arrow_image_view.setImageDrawable(detail_arrow);
+
+
+        detail_arrow_image_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Product.this, ProductDetail.class);
+                intent.putExtra("price", myPrice);
+                intent.putExtra("piece", myPiece);
+                intent.putExtra("productName", myProductName);
+
+                startActivity(intent);
+            }
+        });
 
         newProduct.addView(detail_arrow_image_view);
 
         parent.addView(newProduct);
 
         parent.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-
-
-
-
-
-        /*
-        //children of parent linearlayout
-
-        ImageView iv = new ImageView(getApplicationContext());
-
-        LinearLayout layout2 = new LinearLayout(getApplicationContext());
-
-        layout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        layout2.setOrientation(LinearLayout.VERTICAL);
-
-        parent.addView(iv);
-        parent.addView(layout2);
-
-        //children of layout2 LinearLayout
-
-
-        TextView tv2 = new TextView(getApplicationContext());
-        TextView tv3 = new TextView(getApplicationContext());
-        TextView tv4 = new TextView(getApplicationContext());
-
-        layout2.addView(tv1);
-        layout2.addView(tv2);
-        layout2.addView(tv3);
-        layout2.addView(tv4);*/
     }
 }
--- app/src/main/java/com/ceyizlistesi/ceyizlistesi/ProductDetail.java
+++ app/src/main/java/com/ceyizlistesi/ceyizlistesi/ProductDetail.java
@@ -2,56 +2,81 @@ package com.ceyizlistesi.ceyizlistesi;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.URI;
+
 public class ProductDetail extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-    ImageView productAddButton, cancelButton, demoView;
+    ImageView productAddButton, cancelButton;
     EditText informationEditText;
     Switch privacySwitch;
     Boolean checkedFlag = false;
-    LinearLayout addPictureLinearLayout, pieceLinearLayout, priceLinearLayout, switchLayout, parentLayout,
-            rowLinearLayout,priceRow,pieceRow;
+    LinearLayout addPictureLinearLayout,addnewPictureLinearLayout, pieceLinearLayout, priceLinearLayout, switchLayout, parentLayout,
+            rowLinearLayout,priceRow,pieceRow,picturesLinearLayout;
+    HorizontalScrollView picturesScrollView;
     int piece, price;
-    TextView pieceTextView,priceTextView;
+    String productName;
+    TextView pieceTextView,priceTextView,productNameTextView;
+    int numberOfImage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
 
+
+        price =  getIntent().getIntExtra("price", 0);
+        piece = getIntent().getIntExtra("piece", 0);
+        productName = getIntent().getStringExtra("productName");
+
         cancelButton = findViewById(R.id.cancel_button);
         productAddButton = findViewById(R.id.product_add_button);
         informationEditText = findViewById(R.id.product_information);
         privacySwitch = findViewById(R.id.switch_private);
         addPictureLinearLayout = findViewById(R.id.add_picture_linear_layout);
+        picturesScrollView = findViewById(R.id.pictures_scrollview);
+        addnewPictureLinearLayout = findViewById(R.id.add_new_picture_linear_layout);
         pieceLinearLayout = findViewById(R.id.piece_linear_layout);
         priceLinearLayout = findViewById(R.id.price_linear_layout);
         switchLayout = findViewById(R.id.switch_layout_user);
-        demoView = findViewById(R.id.demo_image_view);
+        productNameTextView = findViewById(R.id.product_name);
         parentLayout = findViewById(R.id.product_detail_parent);
         rowLinearLayout = findViewById(R.id.rows_linear_layout);
         priceRow = findViewById(R.id.price_row);
         pieceRow = findViewById(R.id.piece_row);
         pieceTextView = findViewById(R.id.piece_textView);
         priceTextView = findViewById(R.id.price_textView);
+        picturesLinearLayout = findViewById(R.id.pictures_linear_layout);
 
         parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,9 +85,34 @@ public class ProductDetail extends AppCompatActivity implements CompoundButton.O
             }
         });
 
-        rowLinearLayout.setVisibility(View.GONE);
-        pieceRow.setVisibility(View.GONE);
-        priceRow.setVisibility(View.GONE);
+        picturesScrollView.setVisibility(View.GONE);
+
+
+        if(piece==0 && price == 0){
+            pieceRow.setVisibility(View.GONE);
+            rowLinearLayout.setVisibility(View.GONE);
+            priceRow.setVisibility(View.GONE);
+        }
+        else if(price!=0 && piece ==0){
+            pieceRow.setVisibility(View.GONE);
+            priceLinearLayout.setVisibility(View.GONE);
+            priceTextView.setText(String.valueOf(price+" ₺"));
+        }
+        else if (price==0 && piece !=0){
+            priceRow.setVisibility(View.GONE);
+            pieceLinearLayout.setVisibility(View.GONE);
+            pieceTextView.setText(String.valueOf(piece+" adet"));
+        }
+        else{
+            priceRow.setVisibility(View.VISIBLE);
+            pieceLinearLayout.setVisibility(View.GONE);
+            pieceRow.setVisibility(View.VISIBLE);
+            priceLinearLayout.setVisibility(View.GONE);
+            priceTextView.setText(String.valueOf(price+" ₺"));
+            pieceTextView.setText(String.valueOf(piece+" adet"));
+        }
+
+        productNameTextView.setText(productName);
 
 
         privacySwitch.setOnCheckedChangeListener(this);
@@ -70,6 +120,8 @@ public class ProductDetail extends AppCompatActivity implements CompoundButton.O
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(ProductDetail.this,Product.class);
+                startActivity(intent);
                 finish();
             }
         });
@@ -100,17 +152,26 @@ public class ProductDetail extends AppCompatActivity implements CompoundButton.O
             }
         });
 
+        addnewPictureLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);//one can be replaced with any action cod
+            }
+        });
+
         pieceLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createPieceDialogBox(0);
+                createPieceDialogBox(piece);
             }
         });
 
         priceLinearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createPriceDialogBox(2);
+                createPriceDialogBox(price);
             }
         });
 
@@ -129,6 +190,14 @@ public class ProductDetail extends AppCompatActivity implements CompoundButton.O
         });
 
 
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(ProductDetail.this,Product.class);
+        startActivity(intent);
+        finish();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
@@ -137,14 +206,28 @@ public class ProductDetail extends AppCompatActivity implements CompoundButton.O
             case 0:
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData();
-                    demoView.setImageURI(selectedImage);
+                    //demoView.setImageURI(selectedImage);
+                    createProduct(picturesLinearLayout,selectedImage);
+                    numberOfImage++;
+                    if(addPictureLinearLayout.getVisibility()==View.VISIBLE){
+                        addPictureLinearLayout.setVisibility(View.GONE);
+                        picturesScrollView.setVisibility(View.VISIBLE);
+                    }
                 }
 
                 break;
             case 1:
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData();
-                    demoView.setImageURI(selectedImage);
+
+                    createProduct(picturesLinearLayout,selectedImage);
+
+                    numberOfImage++;
+                    if(addPictureLinearLayout.getVisibility()==View.VISIBLE){
+                        addPictureLinearLayout.setVisibility(View.GONE);
+                        picturesScrollView.setVisibility(View.VISIBLE);
+                    }
+
                 }
                 break;
         }
@@ -329,5 +412,97 @@ public class ProductDetail extends AppCompatActivity implements CompoundButton.O
 
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
+    }
+
+    public void createProduct(final LinearLayout parent, Uri newImage){
+
+        final float scale = getApplicationContext().getResources().getDisplayMetrics().density;
+        int pixels = (int) (30 * scale + 0.5f);
+        int pixels60 = (int) (60 * scale + 0.5f);
+        int pixels80 = (int) (80 * scale + 0.5f);
+        int pixels120 = (int) (120 * scale + 0.5f);
+
+        final FrameLayout newImageFrameLayout = new FrameLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.setMargins(5,0,20,0);
+        newImageFrameLayout.setLayoutParams(layoutParams);
+
+
+
+
+        final ImageView imageView = new ImageView(getApplicationContext());
+        Drawable photoTrash = getResources().getDrawable(R.drawable.ic_trash_photo);
+        imageView.setImageDrawable(photoTrash);
+        LinearLayout.LayoutParams imageParams  = new LinearLayout.LayoutParams( pixels , pixels);
+        imageParams.setMargins(pixels60,0,0,0);
+        imageParams.gravity = Gravity.RIGHT;
+        imageParams.weight=1.0f;
+        imageView.setLayoutParams(imageParams);
+
+        final ImageView imageView2 = new ImageView(getApplicationContext());
+
+        try {
+            Bitmap roundedImage =  MediaStore.Images.Media.getBitmap(this.getContentResolver(), newImage);
+            Bitmap resized = Bitmap.createScaledBitmap(roundedImage, 80, 120, true);
+            resized = roundCorner(resized,10);
+            imageView2.setImageBitmap(resized);
+            LinearLayout.LayoutParams imageParams2  = new LinearLayout.LayoutParams( pixels80 , pixels120);
+            imageParams2.setMargins(1,50,1,1);
+            imageView2.setLayoutParams(imageParams2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        newImageFrameLayout.addView(imageView2);
+        newImageFrameLayout.addView(imageView);
+
+        parent.addView(newImageFrameLayout);
+
+        parent.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberOfImage--;
+                parent.removeView(newImageFrameLayout);
+                if (numberOfImage==0){
+                    addPictureLinearLayout.setVisibility(View.VISIBLE);
+                    picturesScrollView.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    public static Bitmap roundCorner(Bitmap src, float round) {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create bitmap output
+        Bitmap result = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        // set canvas for painting
+        Canvas canvas = new Canvas(result);
+        canvas.drawARGB(0, 0, 0, 0);
+
+        // config paint
+        final Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+
+        // config rectangle for embedding
+        final Rect rect = new Rect(0, 0, width, height);
+        final RectF rectF = new RectF(rect);
+
+        // draw rect to canvas
+        canvas.drawRoundRect(rectF, round, round, paint);
+
+        // create Xfer mode
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        // draw source image to canvas
+        canvas.drawBitmap(src, rect, rect, paint);
+
+        // return final image
+        return result;
     }
 }
